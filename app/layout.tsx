03a8ba46992283911e6b95cdc4ff65276874ad40
zinglea <nguyenmanh081296@gmail.com>
2026-03-02 00:18:51 +0700

    Cập nhật Google Analytics và Cải tiến hệ thống xem PDF

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: 'Website tuyển sinh của Công an tỉnh Cao Bằng - Phòng Tổ chức cán bộ',
 }
 
-import Script from 'next/script'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({
   children,
@@ -18,28 +18,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="vi">
-      <head>
-        {/* Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-TWPX1R9F2P`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-TWPX1R9F2P', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-      </head>
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-TWPX1R9F2P" />
     </html>
   )
 }
